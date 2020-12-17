607d753519d41b94a5d73cad55767a71441c5696
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-17 16:33:33 +0100

    port changed

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ const router= require('./routers/router');
 
 require("dotenv").config();
 
-const port = 5002;
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+//   }
+
 app.use(express.json());
 app.use('/', router);
 
 connectDB();
 
-app.listen(port,()=>{
-    console.log(`I am listening to ${port}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
 });
